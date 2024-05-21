e5b1b4549cdff5769e76eacb9f22fd09bac1657a
vanmanDev <vanmandev@gmail.com>
2024-05-21 16:49:10 +0700

    Add files via upload

--- users/models.py
+++ users/models.py
@@ -17,6 +17,7 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     role = models.CharField(max_length=150, blank=True)
     is_staff = models.BooleanField(_('staff'), default=False)
     is_logged_in = models.BooleanField(_('logged in'), default=False)
+    is_activated = models.BooleanField(_('activated'), default=True)
 
     objects = CustomUserManager()
 
--- users/serializers.py
+++ users/serializers.py
@@ -4,4 +4,4 @@ from .models import CustomUser
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ['id','username', 'email', 'first_name', 'last_name', 'role', 'is_staff', 'is_logged_in']
+        fields = ['id','username', 'email', 'first_name', 'last_name', 'role', 'is_logged_in']
