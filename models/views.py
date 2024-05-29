9d281762490892bc7c14baeff9c800acb24e932b
vanmanDev <vanmandev@gmail.com>
2024-05-30 00:28:58 +0700

    Update views.py

--- models/views.py
+++ models/views.py
@@ -69,80 +69,99 @@ class LeaveRequestList(generics.ListCreateAPIView):
                 leave_request = serializer.save()
                 supervisor = leave_request.user.supervisor
                 if supervisor and supervisor.email:
-                    datetime_start_formatted = leave_request.datetime_start.strftime("Date: %d %B %Y Time: %I:%M:%S %p")
-                    datetime_end_formatted = leave_request.datetime_end.strftime("Date: %d %B %Y Time: %I:%M:%S %p")
-                    duration = (leave_request.datetime_end - leave_request.datetime_start)
-                    days = duration.days
-                    hours, remainder = divmod(duration.seconds, 3600)
-                    minutes, _ = divmod(remainder, 60)
-                    first_name = leave_request.user.first_name
-                    last_name = leave_request.user.last_name
-
-                    email_html_message = f"""
-                    <!DOCTYPE html>
-                    <html>
-                    <head>
-                        <title>Leave Request</title>
-                        <style>
-                            /* Inline CSS styles */
-                            body {{
-                                font-family: Arial, sans-serif;
-                                background-color: #f4f4f4;
-                                margin: 0;
-                                padding: 0;
-                            }}
-                            .container {{
-                                max-width: 800px;
-                                margin: 0 auto;
-                                padding: 20px;
-                                background-color: #fff;
-                                border-radius: 10px;
-                                box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-                            }}
-                            h1 {{
-                                color: #333;
-                            }}
-                            p {{
-                                color: #555;
-                            }}
-                            .details {{
-                                margin-top: 20px;
-                                padding-top: 10px;
-                                border-top: 1px solid #ccc;
-                            }}
-                        </style>
-                    </head>
-                    <body>
-                        <div class="container">
-                            <h1>Leave Request</h1>
-                            <p>Intern <strong>{leave_request.user.first_name} {leave_request.user.last_name}</strong> has requested leave:</p>
-                            <div class="details">
-                                <p><strong>Requested Period:</strong> <strong>[ {datetime_start_formatted} ]</strong> to <strong>[ {datetime_end_formatted} ]</strong></p>
-                                <p><strong>Duration:</strong> {days} days, {hours} hours, {minutes} minutes</p>
-                                <p><strong>Type of Leave:</strong> {leave_request.get_type_of_leave_display()}</p>
-                                <p><strong>Reason:</strong> {leave_request.description}</p>
-                                <p><strong>Contact:</strong> {leave_request.tel}</p>
-                            </div>
-                        </div>
-                    </body>
-                    </html>
-                    """
-                    send_mail(
-                        f"Leave Request from {first_name} {last_name}",
-
-                        "",
-
-                        "VANMAN System",
-
-                        html_message=email_html_message,
-
-                        recipient_list=[supervisor.email],
-
-                        fail_silently=False,
-                    )
+                    self.send_leave_request_email(leave_request, supervisor.email, "Leave Request")
             except Exception as e:
                 logger.error(f"Error during leave request creation: {str(e)}")
                 raise e
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        leave_request = serializer.instance
+        supervisor = leave_request.user.supervisor
+        if supervisor and supervisor.email:
+            self.send_leave_request_email(leave_request, supervisor.email, "Updated Leave Request")
+        
+        return Response(serializer.data)
+
+    def perform_update(self, serializer):
+        with transaction.atomic():
+            try:
+                serializer.save()
+            except Exception as e:
+                logger.error(f"Error during leave request update: {str(e)}")
+                raise e
+
+    def send_leave_request_email(self, leave_request, supervisor_email, subject):
+        datetime_start_formatted = leave_request.datetime_start.strftime("Date: %d %B %Y Time: %I:%M:%S %p")
+        datetime_end_formatted = leave_request.datetime_end.strftime("Date: %d %B %Y Time: %I:%M:%S %p")
+        duration = (leave_request.datetime_end - leave_request.datetime_start)
+        days = duration.days
+        hours, remainder = divmod(duration.seconds, 3600)
+        minutes, _ = divmod(remainder, 60)
+        first_name = leave_request.user.first_name
+        last_name = leave_request.user.last_name
+
+        email_html_message = f"""
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>{subject}</title>
+            <style>
+                body {{
+                    font-family: Arial, sans-serif;
+                    background-color: #f4f4f4;
+                    margin: 0;
+                    padding: 0;
+                }}
+                .container {{
+                    max-width: 800px;
+                    margin: 0 auto;
+                    padding: 20px;
+                    background-color: #fff;
+                    border-radius: 10px;
+                    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+                }}
+                h1 {{
+                    color: #333;
+                }}
+                p {{
+                    color: #555;
+                }}
+                .details {{
+                    margin-top: 20px;
+                    padding-top: 10px;
+                    border-top: 1px solid #ccc;
+                }}
+            </style>
+        </head>
+        <body>
+            <div class="container">
+                <h1>{subject}</h1>
+                <p>Intern <strong>{first_name} {last_name}</strong> has requested leave:</p>
+                <div class="details">
+                    <p><strong>Requested Period:</strong> <strong>[ {datetime_start_formatted} ]</strong> to <strong>[ {datetime_end_formatted} ]</strong></p>
+                    <p><strong>Duration:</strong> {days} days, {hours} hours, {minutes} minutes</p>
+                    <p><strong>Type of Leave:</strong> {leave_request.get_type_of_leave_display()}</p>
+                    <p><strong>Reason:</strong> {leave_request.description}</p>
+                    <p><strong>Contact:</strong> {leave_request.tel}</p>
+                </div>
+            </div>
+        </body>
+        </html>
+        """
+        send_mail(
+            subject,
+            "",
+            "VANMAN System",
+            html_message=email_html_message,
+            recipient_list=[supervisor_email],
+            fail_silently=False,
+        )
     
     def delete(self, request, *args, **kwargs):
         try:
