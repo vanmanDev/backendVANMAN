e3547c069d8573bab6cfb5003d596525701d9d10
vanmanDev <vanmandev@gmail.com>
2024-06-04 21:24:37 +0700

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('users/', views.CustomUserList.as_view()),
+    path('users/<int:pk>/', views.CustomUserDetail.as_view()),
     path('reset_password/', include('django_rest_passwordreset.urls', namespace='reset_password')),
 ]
